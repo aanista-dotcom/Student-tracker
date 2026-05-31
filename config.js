4fb2c223b4d67a2e7c95d26f30544b74ffd5d852
aanista-dotcom <aanista-dotcom@users.noreply.github.com>
2026-05-31 11:48:22 +0530

    Connect frontend to Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  persistenceMode: "local",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  persistenceMode: "supabase",
+  supabaseUrl: "https://almhhzssdirqlmznsmkt.supabase.co",
+  supabaseAnonKey: "sb_publishable_rB1-oNZGgEeSYlBPZXopkQ_Vm0qj3NB",
   supabaseTable: "student_progress_entries",
 };
